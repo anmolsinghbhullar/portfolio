a4d4a7bc43a7d6284fbaf045f89362030357aa30
Anmol <bhullar_anmol@live.ca>
2023-07-17 19:45:15 -0400

    did some setup work on landing page. left to create text transition and adaptability between different screen sizes

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,29 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cardClicked, setCardClicked] = useState(true);
 
+  const handleClick = () => {
+    setCardClicked(!cardClicked);
+  }
+
+  // TODO: implement transition state between two button text states
+  // TODO: static gif becomes something new organized and tidy once clicked (we can remove ability to go back and forth once this happens)
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <header>
+        <Header />
+      </header>
+      <div className='static'>
+        <div className="card">
+          <button onClick={handleClick}>
+            {cardClicked ? '.help({user: me}, {action: filter_thoughts()}))' : 'creating_your_next_idea()'}
+          </button>
+        </div>        
       </div>
-      <h1>My Showcase</h1>
-      <h4>(i think theyre pretty cool)</h4>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          <a className='hover-link'>Hire me!</a>
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Some card style showcasing of each project along with tags of the 
-        frameworks/languages I used to create them.
-      </p>
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+    return (
+        <header className='header'>
+            <h1 className='header-title'>Anmol's Portfolio</h1>
+            <nav className="header-nav">
+                <ul className="nav-list">
+                <li className="nav-item"><a href="#home">Download CV</a></li>
+                <li className="nav-item">
+                    <a href="#projects">Projects</a>
+                    <ul className="dropdown-menu">
+                    <li className="dropdown-item"><a href="#project1">Project 1</a></li>
+                    <li className="dropdown-item"><a href="#project2">Project 2</a></li>
+                    <li className="dropdown-item"><a href="#project3">Project 3</a></li>
+                    </ul>
+                </li>
+                <li className="nav-item"><a href="#about">About</a></li>
+                <li className="nav-item"><a href="#contact">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
